01e61f68ec68a46e665a5ccd1fc8f5fd49983d35
Michael Hartley <mtjhartley@gmail.com>
2017-06-22 10:11:35 -0700

    Added functionality for editing project notes and client notes, created new routes to do so.

--- dojoassignments/python/django/full_stack_django/client_resources/apps/resources/urls.py
+++ dojoassignments/python/django/full_stack_django/client_resources/apps/resources/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     url(r'^client/add$', views.client_add, name='client_add'),
     url(r'^client/handle_add$', views.handle_client_add, name='handle_client_add'),
     url(r'^client/(?P<client_id>\d+)$', views.client_show, name='client_show'),
+    url(r'^client/(?P<client_id>\d+)/edit$', views.client_edit, name='client_edit'),
+    url(r'^client/(?P<client_id>\d+)/handle_edit$', views.handle_client_edit, name='handle_client_edit'),
     url(r'^client/(?P<client_id>\d+)/addproject$', views.project_add, name='project_add'),
     url(r'^client/(?P<client_id>\d+)/handle_add$', views.handle_project_add, name='handle_project_add'),
     url(r'^show/projects/(?P<project_id>\d+)$', views.project_show, name='project_show'),
+    url(r'^edit/projects/(?P<project_id>\d+)$', views.project_edit, name='project_edit'),
+    url(r'^edit/projects/(?P<project_id>\d+)/handle_edit$', views.handle_project_edit, name='handle_project_edit'),
 ]
--- dojoassignments/python/django/full_stack_django/client_resources/apps/resources/views.py
+++ dojoassignments/python/django/full_stack_django/client_resources/apps/resources/views.py
@@ -28,6 +28,25 @@ def client_show(request, client_id):
     }
     return render(request, 'resources/client_show.html', context)
 
+def client_edit(request, client_id):
+    context = {
+        "client": Client.objects.get(id=client_id)
+    }
+    return render(request, 'resources/client_edit.html', context)
+
+def handle_client_edit(request, client_id):
+    if request.method == "POST":
+        notes = request.POST['notes']
+        client = Client.objects.get(id=client_id)
+        client.notes = notes
+        client.save()
+        url = reverse('client_show', kwargs={"client_id": client.id})
+        return HttpResponseRedirect(url)
+    else:
+        return redirect(reverse('index'))
+
+
+
 def project_add(request, client_id):
     context = {
         "client": Client.objects.get(id=client_id)
@@ -53,3 +72,22 @@ def project_show(request, project_id):
 
     }
     return render(request, 'resources/project_show.html', context)
+
+def project_edit(request, project_id):
+    context = {
+        "project": Project.objects.get(id=project_id),
+        "client": Client.objects.get(projects__id=project_id) #foreign keys!
+
+    }
+    return render(request, 'resources/project_edit.html', context)
+
+def handle_project_edit(request, project_id):
+    if request.method == 'POST':
+        notes = request.POST['notes']
+        project = Project.objects.get(id=project_id)
+        project.notes = notes
+        project.save()
+        url = reverse('project_show', kwargs={'project_id': project.id})
+        return HttpResponseRedirect(url)
+    else:
+        return redirect(reverse('index'))
