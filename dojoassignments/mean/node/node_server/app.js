e90713bb3ca4141cfac8c3c7d27c282cd4c32d1a
Michael Hartley <mtjhartley@gmail.com>
2017-07-05 12:51:27 -0700

    adding node folder and bower/jquery commits

---
+++ dojoassignments/mean/node/node_server/app.js
@@ -0,0 +1,34 @@
+//get the http module
+var http = require('http')
+//fs module allows us to read and write content for responses
+var fs = require('fs')
+
+// create a server using a http module
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log("client request URL:", request.url);
+    // this is how we do routing!
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'}); //send data about response
+            response.write(contents); //send response body
+            response.end(); //finished
+        });
+    }
+    else if(request.url === '/stylesheets/style.css') {
+        fs.readFile('stylesheets/style.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/css'}); //send data about response
+            response.write(contents); //send response body
+            response.end(); //finished
+        });
+    }
+    // else request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!');
+    }
+});
+// tell your server which port to run on.
+server.listen(6789);
+// print to terminal window
+console.log("Runnign in localhost at port 6789");
