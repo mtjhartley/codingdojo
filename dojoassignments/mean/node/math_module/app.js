247a0b6f7694777cd0c4dbd56185ffbd05ac484b
Michael Hartley <mtjhartley@gmail.com>
2017-07-05 14:43:19 -0700

    created mathlibrary and used require to bring it to app.js

---
+++ dojoassignments/mean/node/math_module/app.js
@@ -0,0 +1,7 @@
+var my_math = require('./mathlib')();
+console.log(my_math)
+
+my_math.add(3,4);
+my_math.multiply(3,4);
+my_math.square(8)
+my_math.random(1,10)
---
+++ dojoassignments/mean/node/math_module/mathlib.js
@@ -0,0 +1,19 @@
+module.exports = function() {
+    return {
+        add: function(num1, num2) {
+            console.log(num1+num2)
+        },
+        multiply: function(num1, num2) {
+            console.log(num1*num2)
+
+        },
+        square: function(num) {
+            console.log(num*num)
+
+        },
+        random: function(num1, num2) {
+            console.log(Math.floor(Math.random()*num2 + num1))
+
+        },
+    }
+}
