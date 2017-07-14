17ec8c00e880018826624e88189415e7e0f988fa
Michael Hartley <mtjhartley@gmail.com>
2017-07-13 21:17:42 -0700

    powerLevels dynamically updates multiple divs with parent child inheritance in angular.

---
+++ dojoassignments/mean/angular/powerLevels/src/app/power/supersaiyan/supersaiyan.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-supersaiyan',
+  templateUrl: './supersaiyan.component.html',
+  styleUrls: ['./supersaiyan.component.css']
+})
+export class SupersaiyanComponent implements OnInit {
+  @Input() power;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  ngOnChanges(){
+    this.power = this.power * 90;
+  }
+
+}
