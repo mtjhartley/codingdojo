b48486c906bee269cd4705109327c82fd86139a7
Michael Hartley <mtjhartley@gmail.com>
2017-07-31 14:14:20 -0700

    added boxing/unboxing assignment that deals with type casting

---
+++ dojoassignments/netCore/boxing_unboxing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<object> myBox = new List<object>();
+            myBox.Add(7);
+            myBox.Add(28);
+            myBox.Add(-1);
+            myBox.Add(true);
+            myBox.Add("chair");
+
+            foreach(object item in myBox)
+            {
+                Console.WriteLine("This box contains: " + item);
+            }
+
+            int sum = 0;
+            foreach(object item in myBox)
+            {
+                if(item is int)
+                {
+                    sum += (int)item; //using explicit casting, not safe explicit casting, because integers are not nullable
+                }
+
+            }
+            Console.WriteLine("The total sum is {0}", sum);
+
+
+
+        }
+    }
+}
