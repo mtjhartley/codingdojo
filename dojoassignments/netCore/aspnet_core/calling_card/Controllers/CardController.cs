8b9374ccb486595bcb7ea722d7d72d6fbf48affa
Michael Hartley <mtjhartley@gmail.com>
2017-08-03 13:31:01 -0700

    Calling card assignment returns a json object with the aspnetcore framework when the user hits the root url.

---
+++ dojoassignments/netCore/aspnet_core/calling_card/Controllers/CardController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calling_card.Controllers
+{
+    public class CardController : Controller
+    {
+        [HttpGetAttribute]
+        [Route("index")]
+
+        public string Index()
+        {
+            return "Hello Calling Card!";
+        }
+
+        [HttpGet]
+        [Route("")]
+        public JsonResult CallingCard()
+        {
+            var AnonObject = new {
+                                FirstName = "Michael",
+                                LastName = "Hartley",
+                                Age = 23,
+                                favoriteColor = "Green"
+            };
+            return Json(AnonObject);
+        }
+    }
+}
