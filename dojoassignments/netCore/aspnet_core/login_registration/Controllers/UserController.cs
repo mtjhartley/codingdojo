60d78a4bc15df76a6907fa02b7a49cd4c4d428b8
Michael Hartley <mtjhartley@gmail.com>
2017-08-08 12:48:35 -0700

    finished assignment, registration page rerenders the view on failure but that's okay with aspnet.

--- dojoassignments/netCore/aspnet_core/login_registration/Controllers/UserController.cs
+++ dojoassignments/netCore/aspnet_core/login_registration/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using login_registration.Models;
+
 
 namespace login_registration.Controllers
 {
@@ -12,6 +14,54 @@ namespace login_registration.Controllers
         public UserController(DbConnector connect)
         {
             _dbConnector = connect;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                System.Console.WriteLine("User is valid!");
+                string query = $"INSERT INTO users (first_name, last_name, email, password) VALUES ('{newUser.FirstName}', '{newUser.LastName}', '{newUser.Email}', '{newUser.Password}')";
+                _dbConnector.Execute(query);
+                //add to db code
+                return RedirectToAction("Success");
+            }
+            System.Console.WriteLine("User is NOT VALID");
+            return View("Index");
+        
+        }
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(string Email, string Password)
+        {
+            string query = $"SELECT * FROM users WHERE (email='{Email}' AND password='{Password}')";
+            var loggedUser = _dbConnector.Query(query);
+            if (loggedUser.Count > 0)
+            {
+                System.Console.WriteLine("Found the user!");
+                return RedirectToAction("Success");
+            }
+            else {
+                System.Console.WriteLine("Couldn't find the user!");
+                return RedirectToAction("Index");
+            }
+            
+
+        }
+        [HttpGet]
+        [Route("success")]
+        public IActionResult Success()
+        {
+            return View();
         }
 
     }
--- dojoassignments/netCore/aspnet_core/login_registration/Models/User.cs
+++ dojoassignments/netCore/aspnet_core/login_registration/Models/User.cs
@@ -7,12 +7,12 @@ namespace login_registration.Models
     {
         [Required(ErrorMessage = "First Name is required")]
         [MinLength(2, ErrorMessage = "First name must be at least 2 characters long")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{2,45}$", ErrorMessage = "Numbers are not allowed")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{2,45}$", ErrorMessage = "Numbers are not allowed in your first name.")]
         public string FirstName { get; set;}
 
         [Required(ErrorMessage = "Last Name is required")]
         [MinLength(2, ErrorMessage = "Last name must be at least 2 characters long")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{2,45}$", ErrorMessage = "Numbers are not allowed")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{2,45}$", ErrorMessage = "Numbers are not allowed in your last name.")]
         public string LastName { get; set;}
 
         [Required(ErrorMessage = "Email is required")]
@@ -21,6 +21,11 @@ namespace login_registration.Models
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
         public string Password { get; set;}
+
+        [Compare("Password", ErrorMessage = "Password confirmation must match Password")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
     }
 }
